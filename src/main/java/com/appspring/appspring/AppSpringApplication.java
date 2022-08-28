c67de27661de8ff5a66f7cf4ada344ee6d607586
Cleyton Santos <CleytonSantosdeMelo@yahoo.com.br>
2022-08-28 15:19:06 -0300

    ajuste.

--- src/main/java/com/appspring/appspring/AppSpringApplication.java
+++ src/main/java/com/appspring/appspring/AppSpringApplication.java
@@ -3,9 +3,13 @@ package com.appspring.appspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
 
 
 @SpringBootApplication
@@ -13,11 +17,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages= {"com.*"})
 @EnableJpaRepositories(basePackages = {"com.appspring.appspring.repository"})
 @EnableTransactionManagement
-public class AppSpringApplication {
+public class AppSpringApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppSpringApplication.class, args);
 		
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppSpringApplication.class);
 	}
 	
 }
