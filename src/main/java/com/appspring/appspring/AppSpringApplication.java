bccd397e87e543cc0bbdbbd2932a31ffd0b469d6
Cleyton Santos <CleytonSantosdeMelo@yahoo.com.br>
2022-08-28 13:07:39 -0300

    ajuste.

--- src/main/java/com/appspring/appspring/AppSpringApplication.java
+++ src/main/java/com/appspring/appspring/AppSpringApplication.java
@@ -2,8 +2,17 @@ package com.appspring.appspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.appspring.appspring.modelo")
+@ComponentScan(basePackages= {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.appspring.appspring.repository"})
+@EnableTransactionManagement
 public class AppSpringApplication {
 
 	public static void main(String[] args) {
