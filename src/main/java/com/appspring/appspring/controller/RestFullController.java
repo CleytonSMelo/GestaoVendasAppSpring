c2584e27a41adfff0e0162119316e37d10239731
Cleyton Santos <CleytonSantosdeMelo@yahoo.com.br>
2022-08-29 10:21:50 -0300

    Ajustes

--- src/main/java/com/appspring/appspring/controller/RestFullController.java
+++ src/main/java/com/appspring/appspring/controller/RestFullController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.appspring.appspring.dto.VerificaStringDto;
 
 @RestController
-@RequestMapping("**/api/verificarvogal/")
+@RequestMapping("**/api/consultarvogal/")
 public class RestFullController {
 	
 	@GetMapping(value = "{string}" , produces = "application/json")
